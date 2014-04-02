412b5fee1781afd4916f4a71140694b81f3e315f
miyauchi <miya@wpist.me>
2014-04-03 02:09:29 +0900

    first commit

---
+++ Vagrantfile
@@ -0,0 +1,29 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  config.vm.box = "chef/ubuntu-13.10"
+
+  config.vm.provider :aws do |aws, override|
+    override.vm.box = "dummy"
+    override.vm.box_url = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
+
+    aws.access_key_id     = ENV['AWS_ACCESS_KEY']
+    aws.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    aws.keypair_name      = ENV['AWS_EC2_KEYPAIR']
+
+    aws.region = "us-east-1"
+    aws.ami = "ami-59a4a230" # Ubuntu12.04.3 LTS 64bit
+
+    aws.instance_type = "t1.micro"
+
+    override.ssh.username = "ubuntu"
+    override.ssh.private_key_path = ENV['AWS_EC2_KEYPAIR_PATH']
+  end
+
+  config.vm.provision :shell, :path => "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+set -ex
+
+CKAN_PKG='python-ckan_2.0_amd64.deb'
+
+apt-get update -y
+
+# apt-get -y install debconf-utils
+# cat <<EOL | debconf-set-selections
+# grub-pc grub-pc/install_devices multiselect /dev/vda
+# grub-pc grub-pc/install_devices_empty boolean false
+# EOL
+# apt-get -y upgrade
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+
+# Installing CKAN from Source
+# http://docs.ckan.org/en/1117-start-new-test-suite/install-from-source.html
+
+# Install the required packages
+
+apt-get install -y python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-6-jdk python-pastescript
+
+# Install CKAN into a Python virtual environment
+
+if [ ! -d /usr/lib/ckan/default ]; then
+    mkdir -p /usr/lib/ckan/default
+fi
+
+chown `whoami` /usr/lib/ckan/default
+virtualenv --no-site-packages /usr/lib/ckan/default
+. /usr/lib/ckan/default/bin/activate
+
+pip install -e 'git+https://github.com/okfn/ckan.git#egg=ckan'
+pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt
+deactivate
+. /usr/lib/ckan/default/bin/activate
+
+# Setup a PostgreSQL database
+
+sudo -u postgres createuser -S -D -R ckan_default
+sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
+install -o root -g root -m 0644 /vagrant/files/pg_hba.conf /etc/postgresql/9.1/main/
+service postgresql restart
+
+# Create a CKAN config file
+
+mkdir -p /etc/ckan/default
+chown -R `whoami` /etc/ckan/
+
+cd /usr/lib/ckan/default/src/ckan
+paster make-config ckan /etc/ckan/default/production.ini
+install -o root -g root -m 0644 /vagrant/files/production.ini  /etc/ckan/default/
+
+# Setup Solr (Single Solr instance)
+
+sudo install -o root -g root -m 0644 /vagrant/files/jetty /etc/default/
+
+if [ -f /etc/solr/conf/schema.xml ]; then
+    mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
+fi
+
+if [ ! -L /etc/solr/conf/schema.xml ]; then
+    ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml
+fi
+
+sudo service jetty restart
+
+# Create database tables
+
+cd /usr/lib/ckan/default/src/ckan
+paster db init -c /etc/ckan/default/production.ini
+
+# Link to who.ini
+
+ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
+
+
+# Create the WSGI Script File
+# http://docs.ckan.org/en/1117-start-new-test-suite/deployment.html
+install -o root -g root -m 0644 /vagrant/files/apache.wsgi /etc/ckan/default/
+
+
+
+
