9ce7010d37fb5c8b4a9d599633a3c55497b5f87a
sawanoboly <sawanoboriyu@higanworks.com>
2015-12-16 17:29:42 +0900

    use sudo for cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,19 +2,21 @@
 
 set -ex
 
-rm -rf /tmp/*
-rm -rf /vagrant
-rm -f /etc/ssh/ssh_host_*
+sudo apt-get clean all
+
+sudo rm -rf /tmp/*
+sudo rm -rf /var/chef
+sudo rm -rf /vagrant
+sudo rm -f /etc/ssh/ssh_host_*
 cd /var/log
-find /var/log/ -type f -name '*.log' -exec cp /dev/null {} \;
-cp /dev/null /var/log/syslog
+sudo find /var/log/ -type f -name '*.log' -exec sudo cp /dev/null {} \;
+sudo cp /dev/null /var/log/syslog
 
-
-yes | cp /dev/null /root/.ssh/authorized_keys
-yes | cp /dev/null /root/.bash_history
+yes | sudo cp /dev/null /root/.ssh/authorized_keys
+yes | sudo cp /dev/null /root/.bash_history
 if [ -d /home/ubuntu ]; then
-  yes | cp /dev/null /home/ubuntu/.ssh/authorized_keys
-  yes | cp /dev/null /home/ubuntu/.bash_history
+  yes | sudo cp /dev/null /home/ubuntu/.ssh/authorized_keys
+  yes | sudo cp /dev/null /home/ubuntu/.bash_history
 fi
 history -c
 
